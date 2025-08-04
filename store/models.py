bab6ea882d3b23057bce52865f8352133b326fec
risv4na <risvanap2202@gmail.com>
2025-08-04 14:26:44 +0530

    first commit

---
+++ store/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+import datetime
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user.username
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=9)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=1000, null=True, blank=True, default="")
+    image = models.ImageField(upload_to='uploads/products')
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=200, default='', blank=True, null=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
